7b6a9dccf87681c126fd2c5827cebdac511655c6
Jaewon Kim <sanvit@sanvit.me>
2022-01-22 15:53:35 +0000

    fix: s3 api version error

--- config/settings.py
+++ config/settings.py
@@ -199,6 +199,8 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get(
     'AWS_STORAGE_BUCKET_NAME', 'blov-images')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'ap-northeast-2')
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Celery Config
